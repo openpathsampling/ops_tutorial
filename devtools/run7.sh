cb100599215063ec5306705627cd6c0d7ae73871
David W.H. Swenson <dwhs@hyperblazer.net>
2021-06-19 22:58:35 -0400

    250 -> 300 in notebook 7 (should pass more often)

--- devtools/run7.sh
+++ devtools/run7.sh
@@ -5,10 +5,10 @@ for iface in 0 1 2 3 4; do
     openpathsampling equilibrate parallel_setup.nc -o equil_${iface}.nc \
         --scheme scheme_${iface} --extra-steps 5 # user does 50
     openpathsampling pathsampling equil_${iface}.nc -o scheme_${iface}.nc \
-        -n 250
+        -n 300
 done
 
 echo "SAAMPLING RETIS"
 openpathsampling equilibrate parallel_setup.nc -o equil_retis.nc \
     --scheme retis --extra-steps 5  # user does 50
-openpathsampling pathsampling equil_retis.nc -o retis.nc -n 2375
+openpathsampling pathsampling equil_retis.nc -o retis.nc -n 2850
